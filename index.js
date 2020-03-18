426634076fc6c2b9e233d35e6bf87e7cd30ba94b
avlosev <avlosev@yandex.ru>
2020-03-18 08:50:42 +0400

    commit first

---
+++ index.js
@@ -0,0 +1,173 @@
+const http = require('http');
+const { Client } = require('telegram-client');
+// const Client = require('./http-telegram-client')
+
+const port = process.env.PORT || 8080;
+
+const client = new Client({
+  apiId: 1138536, 
+  apiHash: '2067da1a700dfc6685c4d0c464117db4'
+});
+
+const server = http.createServer(async (req, res) => {
+  if (req.method === 'POST') {
+    let options = {};
+    switch (req.url) {
+      case '/searchChatMembers':
+        req.on('data', (data) => {
+          options = JSON.parse(data);
+        });
+        req.on('end',async () => {
+          if(!options.chat_id) {
+            res.statusCode = 400;
+            res.end('Bad POST searchChatMembers! Need chat_id field.');
+          } else {
+            await client.request('searchChatMembers', {
+              chat_id: options.chat_id,
+              limit: 10000
+            })
+            .then(result=>{
+              res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+              res.end(JSON.stringify(result))
+            })
+            .catch(error=>{
+              console.log(error);
+              res.writeHead(400, {'Content-Type': 'application/json; charset=utf-8'});
+              res.end(JSON.stringify(error));
+            });
+          }
+        });
+        break;
+      case '/getSupergroupMembers':
+        req.on('data', (data) => {
+          options = JSON.parse(data);
+        });
+        req.on('end',async () => {
+          if(!options.supergroup_id) {
+            res.statusCode = 400;
+            res.end('Bad POST getSupergroupMembers! Need supergroup_id field.');
+          } else {
+            await client.request('getSupergroupMembers', {
+              supergroup_id: options.supergroup_id,
+              limit: 200
+            })
+            .then(result=>{
+              res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+              res.end(JSON.stringify(result))
+            })
+            .catch(error=>{
+              console.log(error);
+              res.writeHead(400, {'Content-Type': 'application/json; charset=utf-8'});
+              res.end(JSON.stringify(error));
+            });
+          }
+        });
+        break;
+      case '/getUser':
+        req.on('data', (data) => {
+          options = JSON.parse(data);
+        });
+        req.on('end',async () => {
+          if(!options.user_id) {
+            res.statusCode = 400;
+            res.end('Bad POST getUser! Need user_id field.');
+          } else {
+            await client.request('getUser', {user_id: options.user_id})
+            .then(result=>{
+              res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+              res.end(JSON.stringify(result))
+            })
+            .catch(error=>{
+              console.log(error);
+              res.writeHead(400, {'Content-Type': 'application/json; charset=utf-8'});
+              res.end(JSON.stringify(error));
+            });
+          }
+        });
+        break;
+      case '/getChat':
+        req.on('data', (data) => {
+          options = JSON.parse(data);
+        });
+        req.on('end',async () => {
+          if(!options.chat_id) {
+            res.statusCode = 400;
+            res.end('Bad POST getChat! Need chat_id field.');
+          } else {
+            await client.request('getChat', {chat_id: options.chat_id})
+            .then(result=>{
+              res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+              res.end(JSON.stringify(result))
+            })
+            .catch(error=>{
+              console.log(error);
+              res.writeHead(400, {'Content-Type': 'application/json; charset=utf-8'});
+              res.end(JSON.stringify(error));
+            });
+          }
+        });
+        break;
+      case '/getBasicGroupFullInfo':
+        req.on('data', (data) => {
+          options = JSON.parse(data);
+        });
+        req.on('end',async () => {
+          if(!options.basic_group_id) {
+            res.statusCode = 400;
+            res.end('Bad POST getBasicGroupFullInfo! Need basic_group_id field.');
+          } else {
+            await client.request('getBasicGroupFullInfo', {basic_group_id: options.basic_group_id})
+            .then(result=>{
+              res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+              res.end(JSON.stringify(result))
+            })
+            .catch(error=>{
+              console.log(error);
+              res.writeHead(400, {'Content-Type': 'application/json; charset=utf-8'});
+              res.end(JSON.stringify(error));
+            });
+          }
+        });
+        break;
+      default:
+        res.statusCode = 400;
+        res.end('Unknow POST method!');
+        break;
+    }
+  } else if(req.method === 'GET') {
+    switch (req.url) {
+      case '/getChats':
+        await client.request('getChats', {
+          offset_order: '9223372036854775807',
+          offset_chat_id: 0,
+          limit: 1000
+        })
+        .then(result=>{
+          res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+          res.end(JSON.stringify(result))
+        })
+        .catch(error=>{
+          console.log(error);
+          res.writeHead(400, {'Content-Type': 'application/json; charset=utf-8'});
+          res.end(JSON.stringify(error));
+        });
+        break;
+      default:
+        res.statusCode = 400;
+        res.end('Unknow GET method!');
+        break;
+    }
+  } else {
+    res.statusCode = 400;
+    res.end('Use POST method!');
+  }
+});
+
+const connect = async () => {
+  console.log(`Start...`);
+  await client.connect('Aleksey Losev', '+79127591157');
+  console.log(`server is listening on ${port}`);
+  server.listen(port);
+}
+
+connect();
